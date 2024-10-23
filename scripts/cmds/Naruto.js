3f18c06eb92ed9a6462768857094ab1718cb0f18
davidmpo <itachidavid30@gmail.com>
2024-10-23 10:29:19 +0100

    Create Naruto.js

---
+++ scripts/cmds/Naruto.js
@@ -0,0 +1,20 @@
+module.exports = {
+	config: {
+			name: "Naruto",
+			version: "1.0",
+			author: "Shibai Otsutsuki",
+			countDown: 5,
+			role: 0,
+			shortDescription: "sarcasm",
+			longDescription: "sarcasm",
+			category: "reply",
+	},
+onStart: async function(){}, 
+onChat: async function({
+	event,
+	message,
+	getLang
+}) {
+	if (event.body && event.body.toLowerCase() == "Naruto") return message.reply("quoi qu'il arrive🤕 un jour 🤒je deviendrais Hokage ☝🌼😇");
+}
+};
